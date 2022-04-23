538efcf55b656b3c7e788138e426e76b30155594
just9krish <just9krish434@gamil.com>
2022-04-23 20:35:18 +0530

    fully completed

--- main.js
+++ main.js
@@ -3,10 +3,27 @@ const input = document.querySelector(".form_input");
 const errorText = document.querySelector(".form_error");
 
 button.addEventListener("click", (e) => {
+    
   e.preventDefault();
 
-  input.classList.add("error");
-  errorText.classList.remove("hidden");
+  let inputText = document.querySelector(".form_input").value;
+
+  const mailRex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+  if (inputText.match(mailRex)) {
+    input.classList.remove("error");
+    errorText.classList.add("hidden");
+    alert("You have subscribed");
+  } else if (inputText === "") {
+    errorText.textContent = "Please enter your email first.";
+    input.classList.add("error");
+    errorText.classList.remove("hidden");
+  } else {
+    errorText.textContent = "Please provide valid email address.";
+    input.classList.add("error");
+    errorText.classList.remove("hidden");
+  }
 
   setTimeout(() => {
     input.classList.remove("error");
